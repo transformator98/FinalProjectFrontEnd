65f89016fbde079b791e05e5b5786a3dcf566e5c
Anastasiia Kolieva <63116890+anastasiia-kolieva@users.noreply.github.com>
2021-04-03 17:55:46 +0300

    added info to AuthPage and AuthForm
    added files AuthPage.module.css and AuthForm.module.css. Also added some first inofrmation to index.js(AuthPage and AuthForm)

--- src/component/AuthPage/index.js
+++ src/component/AuthPage/index.js
@@ -1,1 +1,19 @@
-export { default } from
+import AuthForm from '../AuthForm/index';
+import styles from './AuthPage.module.css';
+
+export default function AuthPage() {
+  return (
+    <>
+      <div>
+        <p>
+          [ We will help you find weak points in knowledge so that you can
+          strengthen it. We will show you what is relevant to know for a QA
+          Engineer and will try to make the learning process more diverse_ ]
+        </p>
+      </div>
+      <div>
+        <AuthForm />
+      </div>
+    </>
+  );
+}
