6759181671bbe081b0bc7eec9ef5cbf0ed9bbe4f
Anastasiia Kolieva <63116890+anastasiia-kolieva@users.noreply.github.com>
2021-04-03 20:05:01 +0300

    added corrections to AuthPage and AuthForm

--- src/component/AuthPage/index.js
+++ src/component/AuthPage/index.js
@@ -1,19 +1,11 @@
-import AuthForm from '../AuthForm/index';
+// import AuthForm from '../AuthForm/index';
+import { NavLink } from 'react-router-dom';
 import styles from './AuthPage.module.css';
 
 export default function AuthPage() {
   return (
-    <>
-      <div>
-        <p>
-          [ We will help you find weak points in knowledge so that you can
-          strengthen it. We will show you what is relevant to know for a QA
-          Engineer and will try to make the learning process more diverse_ ]
-        </p>
-      </div>
-      <div>
-        <AuthForm />
-      </div>
-    </>
+    <div>
+      <NavLink to="/auth" exact></NavLink>
+    </div>
   );
 }
