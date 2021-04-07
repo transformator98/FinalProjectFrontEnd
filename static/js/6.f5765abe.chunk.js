cf1944af36630ddb38dbe00682abed644b6c1f04
Половинка Иван <62117182+transformator98@users.noreply.github.com>
2021-04-07 08:27:45 +0300

    Updates

--- static/js/6.f5765abe.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[6],{216:function(n,o,p){}}]);
-//# sourceMappingURL=6.f5765abe.chunk.js.map
---
+++ static/js/7.8feedb27.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[7],{248:function(n,o,p){}}]);
+//# sourceMappingURL=7.8feedb27.chunk.js.map
