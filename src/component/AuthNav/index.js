201c877e13fbbaec454f7192dacdddd13e058803
Artem Zymovets <artem.zimovets@gmail.com>
2021-04-07 19:26:33 +0300

    AuthNav

--- src/component/AuthNav/index.js
+++ src/component/AuthNav/index.js
@@ -1,35 +1,10 @@
-import icon from '../../icon/sign-out.svg';
-import styles from './AuthNav.module.scss';
-import React, { useEffect, useState } from 'react';
+// import { getLoggedIn } from '../../redux/user/user-selectors';
+// import { useSelector } from 'react-redux';
+import UserMenu from './UserMenu';
+import AuthMenu from './AuthMenu';
+
 export default function AuthHav() {
-  const [name, setName] = useState();
-  const [avatarUrl, setAvatarUrl] = useState();
-
-  useEffect(() => {
-    setName('Artem');
-    setAvatarUrl(
-      'https://s.gravatar.com/avatar/960f3e39e8c8835d3a9d5a37eafb8819?s=250',
-    );
-  }, []);
-
-  return (
-    <div className={styles.authNav}>
-      <img
-        className={styles.avatar}
-        src={avatarUrl}
-        alt="User Avatar"
-        width="30"
-        height="30"
-      />
-      <span className={styles.name}>{name}</span>
-
-      <img
-        className={styles.signOutIcon}
-        src={icon}
-        alt="sign-out"
-        width="16"
-        height="16"
-      />
-    </div>
-  );
+  // const isLoggedIn = useSelector(getLoggedIn);
+  const isLoggedIn = true;
+  return <>{isLoggedIn ? <UserMenu /> : <AuthMenu />}</>;
 }
