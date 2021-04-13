f338d29a03a9fa0d970edcc28b59e90cce16a2af
Malyshko Serhii <malyshkosergiy@gmail.com>
2021-04-13 12:33:55 +0300

    flashback of unauthorized content has been fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { Switch } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
@@ -25,6 +25,7 @@ import { literature, resources } from './views/UsefulInfo/UsefulInfo.json';
 
 // import { getLoggedIn } from './redux/auth/auth-selectors';
 import { authOperations } from './redux/auth';
+import { authSelectors } from './redux/auth';
 
 import('typeface-montserrat');
 
@@ -55,7 +56,12 @@ const NotFoundView = lazy(() =>
 );
 
 export default function App() {
+  const isRefreshingCurrentUser = useSelector(
+    authSelectors.isRefreshingCurrentUser,
+  );
   const dispatch = useDispatch();
+
+  console.log('isRefreshingCurrentUser', isRefreshingCurrentUser);
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
@@ -63,62 +69,67 @@ export default function App() {
 
   return (
     <>
-      <AppBar />
+      <>
+        {isRefreshingCurrentUser ? (
+          <h1>WAIT PLEASE...</h1>
+        ) : (
+          <>
+            <AppBar />
+            <Container>
+              <Suspense fallback={<Loader />}>
+                <Switch>
+                  <PublicRoute exact path={routes.GOOGLE_AUTH_VIEW}>
+                    <Google />
+                  </PublicRoute>
+                  <PublicRoute path={routes.CONTACTS_VIEW}>
+                    <ContactPageView />
+                  </PublicRoute>
+                  <PublicRoute path={routes.AUTH_VIEW} restricted>
+                    <AuthPageView />
+                  </PublicRoute>
+                  <PrivateRoute
+                    path={routes.MAIN_VIEW}
+                    exact
+                    redirectTo={routes.AUTH_VIEW}
+                  >
+                    <MainPageView />
+                  </PrivateRoute>
+                  <PrivateRoute path={routes.TEST_VIEW}>
+                    <TestPageView />
+                  </PrivateRoute>
+                  <PrivateRoute path={routes.RESULT_VIEW}>
+                    <ResultPageView />
+                  </PrivateRoute>
+                  <PrivateRoute path={routes.USEFUL_INFO_VIEW}>
+                    <UsefulInfo literature={literature} resources={resources} />
+                  </PrivateRoute>
+                  <PublicRoute>
+                    <NotFoundView />
+                  </PublicRoute>
+                </Switch>
+              </Suspense>
 
-      <Container>
-        <Suspense fallback={<Loader />}>
-          <Switch>
-            <PublicRoute exact path={routes.GOOGLE_AUTH_VIEW}>
-              <Google />
-            </PublicRoute>
-
-            <PublicRoute path={routes.CONTACTS_VIEW}>
-              <ContactPageView />
-            </PublicRoute>
-
-            <PublicRoute path={routes.AUTH_VIEW} restricted>
-              <AuthPageView />
-            </PublicRoute>
-
-            <PrivateRoute
-              path={routes.MAIN_VIEW}
-              exact
-              redirectTo={routes.AUTH_VIEW}
-            >
-              <MainPageView />
-            </PrivateRoute>
-
-            <PrivateRoute path={routes.TEST_VIEW}>
-              <TestPageView />
-            </PrivateRoute>
-
-            <PrivateRoute path={routes.RESULT_VIEW}>
-              <ResultPageView />
-            </PrivateRoute>
-
-            <PrivateRoute path={routes.USEFUL_INFO_VIEW}>
-              <UsefulInfo literature={literature} resources={resources} />
-            </PrivateRoute>
-
-            <PublicRoute>
-              <NotFoundView />
-            </PublicRoute>
-          </Switch>
-        </Suspense>
-
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </Container>
-      <Footer />
+              <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+              />
+            </Container>
+            <Footer />
+          </>
+        )}
+      </>
+      )
     </>
   );
 }
+
+// {isRefreshingCurrentUser ? (
+//   <Loader />
+// ) : (
