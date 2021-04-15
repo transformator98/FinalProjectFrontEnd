a7c40feb188055705ae9ca07e265daea4af6ed1f
Alex Margorskij <margorskija@gmail.com>
2021-04-15 10:27:45 +0300

    все рендерится + кнопка формы авторизации

--- src/component/AuthForm/SingIn.js
+++ src/component/AuthForm/SingIn.js
@@ -42,7 +42,9 @@ export default function AuthForm() {
             placeholder="E-mail"
             className={styles.input}
           />
-          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          {errors.email && touched.email ? (
+            <div className={styles.errors}>{errors.email}</div>
+          ) : null}
           <Field
             type="password"
             name="password"
@@ -50,12 +52,12 @@ export default function AuthForm() {
             className={styles.input}
           />
           {errors.password && touched.password ? (
-            <div>{errors.password}</div>
+            <div className={styles.errors}>{errors.password}</div>
           ) : null}
 
           <div className={styles.buttonWrapper}>
             <button type="submit" className={styles.buttonSignIn}>
-              Sign in
+              Login
             </button>
             <NavLink
               exact
